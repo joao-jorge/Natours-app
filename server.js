5404617643a5eb4a39dc5e74642bd71f4a65e40d
João Jorge <nuno93jorge@gmail.com>
2021-12-10 10:49:16 +0100

    Modify mongoose

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
+const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 dotenv.config({path: './config.env'})
 const app = require('./app')
 
 port = process.env.PORT
+
+const DB = process.env.DATABASE_LOCAL
+
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then( () => console.log('Connected successfuly'))
 
 app.listen(port, () => {
   console.log(`App running on port ${port}...`)
